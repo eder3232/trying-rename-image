3e3656b5b7194b53a6705c62dcf45bc195e879c6
eder3232 <eldebar.dk@gmail.com>
2023-11-20 09:35:52 -0500

    Excel writer is stable now

--- utils/rename-image.py
+++ utils/rename-image.py
@@ -1,10 +1,10 @@
 import os
 
-carpeta = './imagesBorrar'
+carpeta = "../imageBorrar"
 
 if os.path.exists(carpeta):
     for nombre_archivo in os.listdir(carpeta):
-        nuevo_nombre = "Yelitza" + nombre_archivo
+        nuevo_nombre = "Carrera-cayma-" + nombre_archivo
 
         ruta_original = os.path.join(carpeta, nombre_archivo)
         ruta_nueva = os.path.join(carpeta, nuevo_nombre)
